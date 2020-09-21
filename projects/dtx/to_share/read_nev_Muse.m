e272cfe739257f096c1e29fa1388bb0d616cd091
Paul-Baudin <paul.baudin@icm-institute.org>
2020-09-21 17:13:30 +0200

    use red_neuralynx_nev instead of ft_read_event

--- projects/dtx/to_share/read_nev_Muse.m
+++ projects/dtx/to_share/read_nev_Muse.m
@@ -12,36 +12,36 @@ for ipart = 1:size(MuseStruct,2)
             error('found 0 or several .nev files');
         end
         
-        nev      = ft_read_event(eventname,'eventformat','neuralynx_nev');
+        nev      = read_neuralynx_nev(eventname,'eventformat','neuralynx_nev');
         
         if isempty(nev)
             continue
         end
         
-        %read header to convert timestamp to seconds
+        %read header to convert TimeStamp to seconds
         temp  	 = dir(fullfile(cfg.rawdir, cfg.directorylist{ipart}{idir}, ['*', cfg.LFP.channel{1}, '.ncs']));
         fname 	 = fullfile(cfg.rawdir, cfg.directorylist{ipart}{idir}, temp.name);        
         hdr      = ft_read_header(fname);
         
         %remove spaces and minuses
         for ievent = 1:size(nev,2)
-            nev(ievent).eventstring = strrep(nev(ievent).eventstring, ' ', '_');
-            nev(ievent).eventstring = strrep(nev(ievent).eventstring, '-', '_');
+            nev(ievent).EventString = strrep(nev(ievent).EventString, ' ', '_');
+            nev(ievent).EventString = strrep(nev(ievent).EventString, '-', '_');
         end
       
         %add events to MuseStruct
         cfgtemp                      = [];
-        cfgtemp.editmarkerfile.toadd = unique({nev.eventstring});
+        cfgtemp.editmarkerfile.toadd = unique({nev.EventString});
         MuseStruct                   = editMuseMarkers(cfgtemp, MuseStruct);
         
         for markername = string(unique(cfgtemp.editmarkerfile.toadd))
            for ievent = 1:size(nev,2)
-               if strcmp(nev(ievent).eventstring, markername)
+               if strcmp(nev(ievent).EventString, markername)
                    
                    MuseStruct{ipart}{idir}.markers.(markername).synctime = ft_getopt(MuseStruct{ipart}{idir}.markers.(markername),'synctime', []);
                    MuseStruct{ipart}{idir}.markers.(markername).clock    = ft_getopt(MuseStruct{ipart}{idir}.markers.(markername),'clock', datetime.empty);
                    
-                   marker_time = (nev(ievent).timestamp - hdr.FirstTimeStamp) / hdr.TimeStampPerSample / hdr.Fs;
+                   marker_time = (nev(ievent).TimeStamp - hdr.FirstTimeStamp) / hdr.TimeStampPerSample / hdr.Fs;
                    MuseStruct{ipart}{idir}.markers.(markername).synctime(end+1) = marker_time;
                    
                    if isempty(MuseStruct{ipart}{idir}.starttime)
