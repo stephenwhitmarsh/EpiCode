5c6bf2618450bde244a943f508c563bbf8cc0c7e
Paul-Baudin <paul.baudin@icm-institute.org>
2020-11-24 18:36:54 +0100

    update

--- projects/dtx/tests/test_persyst.m
+++
@@ -1,8 +0,0 @@
-addpath \\lexport\iss01.charpier\analyses\lgi1\Git-Paul\Persyst_to_Brainvision\PersystImportExport1.0\PersystImportExport1.0
-
-datafile = 'C:\Users\paul.baudin\Pictures\Diapos LGI1\Schwa\test_export.lay';
-
-%fieldtrip cannot read those data
-hdr = ft_read_header(datafile);
-
-[hdr, data] = layread(datafile);
