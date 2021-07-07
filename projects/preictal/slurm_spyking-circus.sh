47cde174c9e83d3b799f5e9510eb77837834ce12
COUSYN Louis <louis.cousyn@icm-institute.org>
2021-07-07 17:36:26 +0200

    clean preictal repository

--- projects/preictal/slurm_spyking-circus.sh
+++ projects/preictal/slurm_spyking-circus.sh
@@ -7,6 +7,6 @@
 #SBATCH --chdir=.
 #SBATCH --output=/network/lustre/iss01/charpier/analyses/vn_preictal/scripts/slurm_output/output-%j_%a-%x.txt
 #SBATCH --error=/network/lustre/iss01/charpier/analyses/vn_preictal/scripts/slurm_output/error-%j_%a-%x.txt
-#SBATCH --array=2
+#SBATCH --array=3
 
 eval $(sed -n "$SLURM_ARRAY_TASK_ID"p /network/lustre/iss01/charpier/analyses/vn_preictal/data/preictal_slurm_job_list.txt)
