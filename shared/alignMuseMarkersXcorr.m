f549c9d0fd76378d5f0582587945679a9d77cb19
Stephen Whitmarsh <stephen.whitmarsh@gmail.com>
2021-10-11 10:13:59 +0200

    update plots

--- shared/alignMuseMarkersXcorr.m
+++ shared/alignMuseMarkersXcorr.m
@@ -16,7 +16,7 @@ function [MuseStruct] = alignMuseMarkersXcorr(cfg, MuseStruct, force)
 %
 % config{1}.align.name        = e.g.: {'PSW','FA','ES'}; % names of patterns to align
 % config{1}.align.channel     = e.g.: {'m1pNs_1', 'm1pNs_2', 'm1pNs_4', 'm1pNs_6', 'm1pNs_7', 'm1pNs_8'}; % channels to use for alignment
-% config{1}.align.reref       = e.g.: 'no'; 
+% config{1}.align.reref       = e.g.: 'no';
 % config{1}.align.refmethod   = e.g.: 'bipolar';
 % config{1}.align.latency.PSW = e.g.: [-0.1 2]; % time period to use for alignment
 % config{1}.align.latency.FA  = e.g.: [-0.1 0.4];
@@ -39,8 +39,9 @@ function [MuseStruct] = alignMuseMarkersXcorr(cfg, MuseStruct, force)
 %    along with EpiCode. If not, see <http://www.gnu.org/licenses/>.
 
 % default settings
-write       = ft_getopt(cfg.align, 'write', true);
-cfg.visible = ft_getopt(cfg, 'visible', 'on');
+write           = ft_getopt(cfg.align, 'write', true);
+cfg.visible     = ft_getopt(cfg, 'visible', 'on');
+cfg.plotpages   = ft_getopt(cfg, 'plotpages', false);
 
 
 fname       = fullfile(cfg.datasavedir,[cfg.prefix, 'MuseStruct_alignedXcorr.mat']);
@@ -93,37 +94,37 @@ end
 LFP                     = readLFP(cfgtemp, MuseStruct, true);
 
 for ipart = 1 : size(cfg.directorylist,2)
-
+    
     for markername = string(cfg.align.name)
-
+        
         if isempty(LFP{ipart}.(markername))
             continue
         end
-
+        
         % baseline correct & bipolar rereferencing
         cfgtemp                 = [];
         cfgtemp.demean          = ft_getopt(cfg.align, 'demean', 'yes');
         cfgtemp.reref           = ft_getopt(cfg.align, 'reref', 'no');
         cfgtemp.refmethod       = ft_getopt(cfg.align, 'refmethod', 'bipolar');
         dat                     = ft_preprocessing(cfgtemp, LFP{ipart}.(markername));
-
+        
         % clear LFP for memory
         LFP{ipart}.(markername) = [];
-
+        
         % select data
         if strcmp(cfg.align.latency.(markername), 'all')
             latency = [dat.time{1}(1), dat.time{1}(end)];
         else
             latency = cfg.align.latency.(markername);
         end
-
+        
         cfgtemp                 = [];
         cfgtemp.latency         = latency;
         dat_timesel             = ft_selectdata(cfgtemp, dat);
-
+        
         cfgtemp                 = [];
         dat_avg_orig            = ft_timelockanalysis(cfgtemp, dat);
-
+        
         % put selected timeperiod in single matrix with concatinated channels
         fprintf('Preparing alignment...\n');
         d = size(dat_timesel.trial{1});
@@ -131,15 +132,15 @@ for ipart = 1 : size(cfg.directorylist,2)
         for itrial = 1 : size(dat.trial,2)
             LFP_concatinated(itrial,:) = reshape(dat_timesel.trial{itrial}',1,d(1)*d(2));
         end
-
+        
         % align data to closest-peak in cross correlation
         [shifted, nshift]   = alignXcorr(LFP_concatinated, 10);
-
+        
         % remove those that moved too much
         rejected            = (nshift < mean(nshift) - std(nshift)*3) | (nshift > mean(nshift) + std(nshift)*3);
         shifted_clean       = shifted(~rejected,:);
         nshift_clean        = nshift(~rejected);
-
+        
         % make time-shifted average
         cfgtemp             = [];
         cfgtemp.trials      = find(~rejected);
@@ -148,7 +149,7 @@ for ipart = 1 : size(cfg.directorylist,2)
             dat_shifted.time{itrial} = dat_shifted.time{itrial} + nshift_clean(itrial) / dat_shifted.fsample;
         end
         dat_avg_shifted     = ft_timelockanalysis([], dat_shifted);
-
+        
         % remove rejected trials of non-shifted for plotting later
         cfgtemp             = [];
         cfgtemp.trials      = find(~rejected);
@@ -157,24 +158,24 @@ for ipart = 1 : size(cfg.directorylist,2)
         %% Plot alignment
         fig = figure('visible', cfg.visible);
         fig.Renderer = 'Painters';
-
+        
         subplot(1, 5, 1);
         imagesc(LFP_concatinated(~rejected,:));
         title(sprintf('Cleaned (%d-%d)', sum(~rejected), sum(rejected)));
         set(gca,'xtick', []);
-
+        
         subplot(1, 5, 2);
         imagesc(shifted(~rejected, :));
         title('Aligned');
         set(gca,'xtick', []);
-
+        
         subplot(1, 5, 3);
         scatter(nshift(~rejected) / dat.fsample * 1000, sum(~rejected):-1:1,'k.');
         set(gca,'ytick', []);
         xlabel('ms');
         title('Timeshift');
         axis tight;
-
+        
         subplot(2, 5, [4 5]); hold
         plot(dat_avg_orig.time, dat_avg_orig.avg');
         axis tight;
@@ -182,7 +183,7 @@ for ipart = 1 : size(cfg.directorylist,2)
         patch([latency(1), latency(2), latency(2), latency(1)], [ax(3), ax(3), ax(4), ax(4)], 'r', 'facealpha', 0.1, 'edgecolor', 'none');
         title('Original');
         xlim([dat.time{1}(1), dat.time{1}(end)]);
-
+        
         subplot(2,5,[9 10]); hold
         plot(dat_avg_shifted.time, dat_avg_shifted.avg');
         axis tight
@@ -190,7 +191,7 @@ for ipart = 1 : size(cfg.directorylist,2)
         patch([latency(1), latency(2), latency(2), latency(1)], [ax(3), ax(3), ax(4), ax(4)], 'r', 'facealpha', 0.1, 'edgecolor', 'none');
         title('Aligned');
         xlim([dat.time{1}(1), dat.time{1}(end)]);
-
+        
         set(fig,'Renderer','Painters');
         set(fig,'PaperOrientation','landscape');
         set(fig,'PaperUnits','normalized');
@@ -206,63 +207,69 @@ for ipart = 1 : size(cfg.directorylist,2)
             mkdir(fullfile(cfg.imagesavedir, cfg.prefix(1:end-1)));
         end
         
-        print(fig, '-dpng', fullfile(cfg.imagesavedir, cfg.prefix(1:end-1), strcat(cfg.prefix, 'p', num2str(ipart), '_', markername, '_alignmentXcorr.png')));
-        print(fig, '-dpdf', fullfile(cfg.imagesavedir, cfg.prefix(1:end-1), strcat(cfg.prefix, 'p', num2str(ipart), '_', markername, '_alignmentXcorr.pdf'))); 
-        close all
-  
+        fname_fig = fullfile(cfg.imagesavedir, 'templates', 'alignment', strcat(cfg.prefix, 'p', num2str(ipart), '_', markername, '_alignmentXcorr.png'));
+        isdir_or_mkdir(fileparts(fname_fig));
+        exportgraphics(fig, fname_fig);
+        
         %% Plot alignment
-        nrchan  = size(dat_shifted.label, 1);
-        nrtrial = size(dat_shifted.trial,2);
-        nrpage  = ceil(nrtrial / 100);
-        
-        for ipage = 1 : nrpage
+        if cfg.plotpages
             
-            % get scaling parameter for all channels
-            maxrange = [];
-            for  ichan = 1 : size(dat_shifted.label, 1)
-                for itrial = 1 : size(dat_shifted.trial, 2)
-                    y           = dat_shifted.trial{itrial}(ichan,:);
-                    maxrange    = max(abs([maxrange, y]));
+            nrchan  = size(dat_shifted.label, 1);
+            nrtrial = size(dat_shifted.trial,2);
+            nrpage  = ceil(nrtrial / 100);
+            
+            for ipage = 1 : nrpage
+                
+                % get scaling parameter for all channels
+                maxrange = [];
+                for  ichan = 1 : size(dat_shifted.label, 1)
+                    for itrial = 1 : size(dat_shifted.trial, 2)
+                        y           = dat_shifted.trial{itrial}(ichan,:);
+                        maxrange    = max(abs([maxrange, y]));
+                    end
                 end
+                maxrange = maxrange / 4;
+                
+                fig             = figure;
+                fig.Renderer    = 'Painters'; % Else pdf is saved to bitmap
+                
+                for ichan = 1 : size(dat_shifted.label, 1)
+                    
+                    subplot(4, nrchan, [ichan ichan+nrchan ichan+nrchan*2 ichan+nrchan*3]); hold;
+                    
+                    i = 0;
+                    for itrial = 1+(ipage-1)*100 : min(nrtrial, 100+(ipage-1)*100)
+                        
+                        plot(dat.time{itrial}, dat.trial{itrial}(ichan, :) + maxrange * i, 'color', [0.5, 0.5, 0.5]);
+                        plot(dat_shifted.time{itrial}, dat_shifted.trial{itrial}(ichan, :) + maxrange * i, 'k');
+                        i = i + 1;
+                    end
+                    
+                    axis tight
+                    ax = axis;
+                    patch([latency(1), latency(2), latency(2), latency(1)], [ax(3), ax(3), ax(4), ax(4)], 'r', 'facealpha', 0.1, 'edgecolor', 'none');
+                    plot([0, 0], [0, maxrange * 100], 'b:');
+                    set(gca,'children',flipud(get(gca,'children')));
+                    set(gca,'Yticklabels','')
+                    xlabel('time (s)');
+                    if ichan == 1
+                        ylabel('trials');
+                    end
+                    title(dat_shifted.label{ichan}, 'interpreter', 'none');
+                    
+                end
+                set(findall(fig, '-property', 'fontsize'), 'fontsize', 8);
+                set(fig,'Renderer','Painters');
+                set(fig,'PaperOrientation','landscape');
+                set(fig,'PaperUnits','normalized');
+                set(fig,'PaperPosition', [0 0 1 1]);
+                
+                fname_fig = fullfile(cfg.imagesavedir, 'templates', 'alignment', strcat(cfg.prefix, 'p', num2str(ipart), '_', markername, '_alignmentXcorr_page', num2str(ipage), '.png'));
+                isdir_or_mkdir(fileparts(fname_fig));
+                exportgraphics(fig, fname_fig);
+                
             end
-            maxrange = maxrange / 4;
-            
-            fig             = figure;
-            fig.Renderer    = 'Painters'; % Else pdf is saved to bitmap
-            
-            for ichan = 1 : size(dat_shifted.label, 1)
-                
-                subplot(4, nrchan, [ichan ichan+nrchan ichan+nrchan*2 ichan+nrchan*3]); hold;
-                
-                i = 0;
-                for itrial = 1+(ipage-1)*100 : min(nrtrial, 100+(ipage-1)*100)
-                    plot(dat.time{itrial}, dat.trial{itrial}(ichan, :) + maxrange * i, 'color', [0.5, 0.5, 0.5]);
-                    plot(dat_shifted.time{itrial}, dat_shifted.trial{itrial}(ichan, :) + maxrange * i, 'k');
-                    i = i + 1;
-                end
-                
-                axis tight
-                plot([0, 0], [0, maxrange * 100], 'b:');
-%                 set(gca,'children',flipud(get(gca,'children')));
-                set(gca,'Yticklabels','')
-                xlabel('time (s)');
-                if ichan == 1
-                    ylabel('trials');
-                end
-                title(dat_shifted.label{ichan}, 'interpreter', 'none');
-
-            end
-            set(findall(fig, '-property', 'fontsize'), 'fontsize', 8);
-            set(fig,'Renderer','Painters');
-            set(fig,'PaperOrientation','landscape');
-            set(fig,'PaperUnits','normalized');
-            set(fig,'PaperPosition', [0 0 1 1]);
-
-            print(fig, '-dpng', fullfile(cfg.imagesavedir, cfg.prefix(1:end-1), strcat(cfg.prefix, 'p', num2str(ipart), '_', markername, '_alignmentXcorr_page', num2str(ipage), '.png')), '-r600');
-            print(fig, '-dpdf', fullfile(cfg.imagesavedir, cfg.prefix(1:end-1), strcat(cfg.prefix, 'p', num2str(ipart), '_', markername, '_alignmentXcorr_page', num2str(ipage), '.pdf')));
-            
         end
-        
         %% correct MuseStruct
         i = 1;
         for idir = 1 : length(MuseStruct{ipart})
@@ -279,7 +286,7 @@ for ipart = 1 : size(cfg.directorylist,2)
                 continue
             end
             todelete = [];
-
+            
             for ievent = 1 : size(MuseStruct{ipart}{idir}.markers.(cfg.muse.startmarker.(markername)).synctime, 2)
                 if any(dat.trialinfo.trialnr == ievent & dat.trialinfo.idir == idir)
                     timeshift = nshift(i) * 1 / dat.fsample;
