ff7df7ad6845ce011315088c7140c53ef8d5d080
Stephen Whitmarsh <stephen.whitmarsh@gmail.com>
2022-02-28 16:01:51 +0100

    update preictal project from Louis's project directory, for Anthony

--- projects/preictal/slurm_preictal_project.sh
+++ projects/preictal/slurm_preictal_project.sh
@@ -7,9 +7,9 @@
 #SBATCH --chdir=.
 #SBATCH --output=/network/lustre/iss01/charpier/analyses/vn_peictal/scripts/slurm_output/output-%j_%a-%x.txt
 #SBATCH --error=/network/lustre/iss01/charpier/analyses/vn_peictal/scripts/slurm_output/error-%j_%a-%x.txt
-#SBATCH --array=3
+#SBATCH --array=1-3
 
 
-module load MATLAB/R2019b
-matlab -nodesktop -softwareopengl -nosplash -nodisplay -r "preictal_project($SLURM_ARRAY_TASK_ID);"
+module load MATLAB/R2020b
+matlab -nodesktop -softwareopengl -nosplash -nodisplay -r "slurm_preictal_project($SLURM_ARRAY_TASK_ID);"
 sleep 5;
